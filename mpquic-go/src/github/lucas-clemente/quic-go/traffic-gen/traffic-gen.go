d3745859341dbd259c9b6a48e510cac4f904d758
vuva <vuanh.vu@ikt.uni-hannover.de>
2019-08-01 13:38:14 +0200

    Revert "multithread with channel"
    This reverts commit f82d636d9dcde895eaf20200545cfd26d473fb91.

--- mpquic-go/src/github/lucas-clemente/quic-go/traffic-gen/traffic-gen.go
+++ mpquic-go/src/github/lucas-clemente/quic-go/traffic-gen/traffic-gen.go
@@ -253,15 +253,11 @@ func startClientMode(address string, protocol string, run_time uint, csize_distr
 	timeStamps := make(map[uint]uint)
 	writeTime := make(map[uint]uint)
 	send_queue := ByteQueue{queue: make([][]byte, 0)}
-	// trafficGenDone := make(chan bool, 1)
+	trafficGenDone := make(chan bool)
 	sendingDone := make(chan bool)
-
 	go func() {
-		for true {
-			// done := <-trafficGenDone
-			// if done {
-			// 	break
-			// }
+		for !<-trafficGenDone {
+
 			if len(send_queue.queue) == 0 {
 				continue
 			}
@@ -282,7 +278,7 @@ func startClientMode(address string, protocol string, run_time uint, csize_distr
 			utils.Debugf("Messages sent. Q size: %d \n", len(send_queue.queue))
 		}
 
-		// sendingDone <- true
+		sendingDone <- true
 	}()
 	go func() {
 		startTime := time.Now()
@@ -300,8 +296,7 @@ func startClientMode(address string, protocol string, run_time uint, csize_distr
 
 			wait(1 / getRandom(arrival_distro, arrival_value))
 		}
-		// trafficGenDone <- true
-		sendingDone <- true
+		trafficGenDone <- true
 	}()
 	<-sendingDone
 	writeToFile(LOG_PREFIX+"client-timestamp.log", timeStamps)
@@ -529,7 +524,7 @@ func main() {
 	flagCsizeDistro := flag.String("csizedist", "c", "data chunk size distribution")
 	flagCsizeValue := flag.Float64("csizeval", 1000, "data chunk size value")
 	flagArrDistro := flag.String("arrdist", "c", "arrival distribution")
-	flagArrValue := flag.Float64("arrval", 1, "arrival value")
+	flagArrValue := flag.Float64("arrval", 1000, "arrival value")
 	flagAddress := flag.String("a", "localhost", "Destination address")
 	flagProtocol := flag.String("p", "tcp", "TCP or QUIC")
 	flagLog := flag.String("log", "", "Log folder")
