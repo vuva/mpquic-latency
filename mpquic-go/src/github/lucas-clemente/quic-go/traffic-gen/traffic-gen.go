e3debf8d99f75276e755fc7ed6a5cce29c73d7a9
Vu Anh Vu <vuanh.vu@ikt.uni-hannover.de>
2019-11-12 11:44:26 +0100

    change ninetail selected path

--- mpquic-go/src/github/lucas-clemente/quic-go/traffic-gen/traffic-gen.go
+++ mpquic-go/src/github/lucas-clemente/quic-go/traffic-gen/traffic-gen.go
@@ -356,13 +356,13 @@ func startQUICServer(addr string, isMultipath bool) error {
 	if err != nil {
 		return err
 	}
-	go sess, err := listener.Accept()
+	sess, err := listener.Accept()
 	if err != nil {
 		return err
 	}
 	defer sess.Close(err)
 
-	go stream, err := sess.AcceptStream()
+	stream, err := sess.AcceptStream()
 	if err != nil {
 		panic(err)
 	}
