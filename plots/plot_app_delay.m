a546b556b3d263f040a5c2ea1887e0a714baaedc
vuva <vuanh.vu@ikt.uni-hannover.de>
2019-11-14 23:02:52 +0100

    timestamp send before sent

--- plots/plot_app_delay.m
+++ plots/plot_app_delay.m
@@ -1,10 +1,10 @@
 %% ====== SET PARAMS ==========
 k=1;
 n=1;
-folder='D:\Work\Data\mp-quic-logs\';
+folder='C:\Work\Data\mp-quic-logs\';
 distribution_name = 'on5-off3';
 global exp_name;
-exp_name = 'app-delay-mptcp-c-10-c-150000';
+exp_name = 'app-delay-mptcp-c-5-c-200000';
 log_surfix= '-timestamp.log';
 pcap_surfix= '-pcap.dat';
 frame_log_surfix= '-frame.log';
