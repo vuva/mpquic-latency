2d22384113e69bd0b2f75e004f8caf382af7bfbc
vuva <vuanh.vu@ikt.uni-hannover.de>
2019-11-22 12:30:33 +0100

    traffic gen client check sending

--- mpquic-go/src/github/lucas-clemente/quic-go/traffic-gen/traffic-gen.go
+++ mpquic-go/src/github/lucas-clemente/quic-go/traffic-gen/traffic-gen.go
@@ -338,6 +338,8 @@ func startClientMode(address string, protocol string, run_time uint, csize_distr
 			// send_queue.mutex.Lock()
 			// send_queue.isSending = true
 			// send_queue.mutex.Unlock()
+
+			utils.Debugf("Time in queue: %d \n", uint(time.Now().Nanosecond())-timeStamps[bytesToInt(message[0:4])])
 			if protocol == "quic" {
 				if isMultiStream {
 					go startQUICClientStream(quic_session, message)
