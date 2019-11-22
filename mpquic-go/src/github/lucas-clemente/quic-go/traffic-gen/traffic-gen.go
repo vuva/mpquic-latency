22a9752bb49036891a6d2050657a0b5a3783babb
vuva <vuanh.vu@ikt.uni-hannover.de>
2019-11-22 16:44:40 +0100

    debug multistream

--- mpquic-go/src/github/lucas-clemente/quic-go/traffic-gen/traffic-gen.go
+++ mpquic-go/src/github/lucas-clemente/quic-go/traffic-gen/traffic-gen.go
@@ -343,7 +343,7 @@ func startClientMode(address string, protocol string, run_time uint, csize_distr
 			// utils.Debugf("Time in queue: %d \n", uint(time.Now().UnixNano())-timeStamps[bytesToInt(message[0:4])])
 			if protocol == "quic" {
 				if isMultiStream {
-					utils.Debugf("OpenStream no.: %d", quic_session.GetOpenStreamNo())
+
 					go startQUICClientStream(quic_session, message)
 				} else {
 					if current_stream == nil {
@@ -408,6 +408,7 @@ func startQUICClientStream(quic_session quic.Session, message []byte) {
 		utils.Debugf("Error OpenStreamSync:", err)
 		return
 	}
+	utils.Debugf("OpenStream no.: %d", quic_session.GetOpenStreamNo())
 	beforeWrite := time.Now()
 	defer stream.Close()
 	stream.Write(message)
