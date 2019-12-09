3f2b82e65057b8236b7f684a9fcba2afaa5c44d4
Vu Anh Vu <vuanh.vu@ikt.uni-hannover.de>
2019-12-09 11:13:37 +0100

    debug ninetail

--- mpquic-go/src/github/lucas-clemente/quic-go/traffic-gen/traffic-gen.go
+++ mpquic-go/src/github/lucas-clemente/quic-go/traffic-gen/traffic-gen.go
@@ -281,7 +281,7 @@ func startClientMode(address string, protocol string, run_time uint, csize_distr
 
 		for i := 1; time.Now().Before(endTime); i++ {
 			send_queue_size := 0
-			for e := send_queue.mess_list.Front(); send_queue.mess_list.Len() > 0 && e != nil; e = e.Next() {
+			for e := send_queue.mess_list.Front(); e.Value != nil; e = e.Next() {
 				send_queue_size += len(e.Value.([]byte))
 			}
 
