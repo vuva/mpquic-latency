eebd13d1b03cc7cea962c93fc03b2543ec94ed97
Vu Anh Vu <vuanh.vu@ikt.uni-hannover.de>
2020-01-02 11:32:55 +0100

    NT: reserved 2đ

--- mpquic-go/src/github/lucas-clemente/quic-go/scheduler.go
+++ mpquic-go/src/github/lucas-clemente/quic-go/scheduler.go
@@ -516,7 +516,7 @@ pathLoop:
 						utils.Debugf("\n Ninetails: redundant send stream %d datainstream %d on path %d", next_stream.streamID, dataInStream, pathID)
 					}
 				}
-			} else if availablePathCount == 1 && selectedPath.pathID != highestRatePath.pathID && !selectedPath.SendingAllowedWithReserved(2*protocol.MaxPacketSize) && !hasRetransmission {
+			} else if availablePathCount == 1 && selectedPath.pathID != highestRatePath.pathID && !selectedPath.SendingAllowedWithReserved(20*protocol.MaxPacketSize) && !hasRetransmission {
 				utils.Debugf("\n Ninetails: shortlink waiting fatlink selectedPath %d highestRatePath %d", selectedPath.pathID, highestRatePathRTT)
 				return nil
 			}
