22657b2fa4aef5f02dd6e711d00c385dc595ad2b
Vu Anh Vu <vuanh.vu@ikt.uni-hannover.de>
2020-01-14 00:19:25 +0100

    BIG UPDATE: redesign ninetail debug

--- mpquic-go/src/github/lucas-clemente/quic-go/scheduler.go
+++ mpquic-go/src/github/lucas-clemente/quic-go/scheduler.go
@@ -605,7 +605,7 @@ pathLoop:
 	shouldRedundant := false
 	if highestRate != 0 {
 		shouldRedundant = dataInStream <= 3*uint64(protocol.MaxPacketSize) || float64(dataInStream*8)/float64(highestRate)*1000.0 < float64(highestRatePathRTT+lowestRTT/2)
-		utils.Debugf("\n Ninetails: selectedPathID %d availablepaths %d \n highestRatepath %d = %d bps, %d ms; \n lowRTTpath %d = %d bps, %d ms \n stream %d datainstream %d with %f >< %d + %d/2 ,shouldRedundant %t", selectedPath.pathID, availablePathCount, highestRatePath.pathID, highestRate, highestRatePathRTT, lowestRTTPath.pathID, lowestRTTPathRate, lowestRTT, next_stream.StreamID(), dataInStream, float64(dataInStream*8)/float64(highestRate)*1000.0, highestRatePathRTT, lowestRTT, shouldRedundant)
+		utils.Debugf("\n Ninetails: selectedPathID %d availablepaths %d \n highestRatepath %d = %d bps, %d ms; \n lowRTTpath %d = %d bps, %d ms \n stream datainstream %d with %f >< %d + %d/2 ,shouldRedundant %t", selectedPath.pathID, availablePathCount, highestRatePath.pathID, highestRate, highestRatePathRTT, lowestRTTPath.pathID, lowestRTTPathRate, lowestRTT, dataInStream, float64(dataInStream*8)/float64(highestRate)*1000.0, highestRatePathRTT, lowestRTT, shouldRedundant)
 		if shouldRedundant {
 			// if availablePathCount > 1 {
 			// 	for pathID, pth := range s.paths {
